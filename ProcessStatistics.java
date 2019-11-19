16b47516ee479d7e46dcf25ce9113e58622f3222
VirusFreeNewt <23226145+VirusFreeNewt@users.noreply.github.com>
2019-11-19 09:18:41 -0700

    First commit

---
+++ ProcessStatistics.java
@@ -0,0 +1,29 @@
+package textstatistics;
+
+import java.io.File;
+
+/**
+ *
+ *
+ *
+ *
+ * @author Pluska
+ */
+public class ProcessStatistics
+{
+
+    public static void main(String[] args)
+    {
+
+        File file = new File(args[0]);
+
+        if (file.exists())
+        {
+
+            TextStatistics textStatistics = new TextStatistics(file);
+
+        }
+
+    }
+
+}
