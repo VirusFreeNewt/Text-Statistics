70e770c8b2054634115271e59d9119450514a3ad
VirusFreeNewt <23226145+VirusFreeNewt@users.noreply.github.com>
2019-11-20 21:18:42 -0700

    Ready for Testing

--- ProcessStatistics.java
+++ ProcessStatistics.java
@@ -14,8 +14,16 @@ public class ProcessStatistics
 
     public static void main(String[] args)
     {
-
-        File file = new File(args[0]);
+        File file = null;
+        try
+        {
+            file = new File(args[0]);
+        }
+        catch (ArrayIndexOutOfBoundsException e)
+        {
+            System.out.println("Please pass in a text file when compiling");
+            System.exit(1);
+        }
 
         if (file.exists())
         {
@@ -23,6 +31,12 @@ public class ProcessStatistics
             TextStatistics textStatistics = new TextStatistics(file);
 
         }
+        else
+        {
+            System.out.println("Please pass in a text file when compiling");
+        }
+
+
 
     }
 
